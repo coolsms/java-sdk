37f7eaea71c2419e1028b4638314353297a843cd
hosy <hosy@nurigo.net>
2016-03-29 21:24:39 +0900

    Group Message Unit Test 추가 CoolsmsUnitTest.java 만 봐주세요

--- src/test/java/CoolsmsUnitTest.java
+++ src/test/java/CoolsmsUnitTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import java.io.Reader;
 import java.util.HashMap;
 
 import net.nurigo.java_sdk.api.GroupMessage;
@@ -8,7 +9,9 @@ import net.nurigo.java_sdk.api.Message;
 import net.nurigo.java_sdk.api.SenderID;
 import net.nurigo.java_sdk.exceptions.CoolsmsException;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.junit.Test;
 
 
@@ -21,13 +24,14 @@ public class CoolsmsUnitTest {
 	Image image = new Image(api_key, api_secret);
 	SenderID senderID;
 	JSONObject result;
+	JSONArray result_array;
+	HashMap<String, String> params = new HashMap<String, String>();
 	
 	
 	@Test
 	public void MessageTest() {		
 		try {			
-			// Send
-			HashMap<String, String> params = new HashMap<String, String>();		
+			// send					
 			params.put("to", "01000000000");
 		    params.put("from", "01000000000");
 		    params.put("type", "SMS");
@@ -37,20 +41,20 @@ public class CoolsmsUnitTest {
 		    result = message.send(params);		    
 		    assertEquals(result.get("result_message"), "Success");
 		    
-			// Balance
+			// balance
 			result = message.balance();
 			assertNotNull(result.get("cash"));			
 			
-			// Sent
+			// sent
 			params.clear();
 			result = message.sent(params);
 			assertNotNull(result.get("data"));
 			
-			// Status
+			// status
 			result = message.getStatus(params);
 			assertNotNull(result.get("data"));
 						
-			// Cancel
+			// cancel
 			params.put("mid", "MIDTEST");
 			result = message.cancel(params);			
 			assertTrue(result.isEmpty());				
@@ -61,12 +65,70 @@ public class CoolsmsUnitTest {
 	
 	@Test
 	public void GroupMessageTest() {
-		
+		try {
+			String group_id;
+			String message_id;
+			int success_count;
+			params.clear();
+			
+			// create group
+			params.put("mode", "test");
+			result = groupMessage.createGroup(params);
+			assertNotNull(result.get("group_id"));
+			group_id = (String) result.get("group_id");		
+			
+			// group info			
+			result = groupMessage.getGroupInfo(group_id);
+			assertNotNull(result.get("message_count"));
+			
+			// group list
+			result = groupMessage.getGroupList();
+			assertNotNull(result.get("list"));
+			
+			// add messages
+			params.clear();
+			params.put("to", "01000000000");
+			params.put("from", "01000000000");
+			params.put("text", "Coolsms Testing Message!");
+			params.put("group_id", group_id); // Group ID	    
+			result = groupMessage.addMessages(params);		
+			message_id = (String) result.get("message_id");
+			success_count = Integer.parseInt(result.get("success_count").toString());
+			assertNotEquals(success_count, 0);
+						
+			// message list
+			params.clear();
+			params.put("group_id", group_id);
+			result = groupMessage.getMessageList(params);
+			assertNotNull(result.get("list"));
+			result_array = (JSONArray) result.get("list");
+			result_array.get(0);
+			message_id = (String) result_array.get(0);			
+			
+			// send
+			result = groupMessage.sendGroupMessage(group_id);
+			assertNotNull(result.get("group_id"));
+			
+			// delete messages
+			result = groupMessage.deleteMessages(group_id, message_id);
+			// 이 부분은 테스트 하면서 고쳐야 할듯 null값으로 넘어옴 
+			
+			// delete group
+			result = groupMessage.deleteGroups(group_id);
+			success_count = Integer.parseInt(result.get("success_count").toString());
+			assertNotEquals(success_count, 0);			
+		} catch (Exception e) {
+			fail(e.toString());
+		}
 	}
 	
 	@Test
 	public void ImageTest() {
-		
+		try {
+
+		} catch (Exception e) {
+
+		}
 	}
 	
 	@Test
