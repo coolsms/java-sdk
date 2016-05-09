d340a4aa596489ee1735876161cc5e3ed4779636
희성 이 <hosy@nurigo.net>
2016-05-09 15:50:34 +0900

    Merged in hosy/coolsms/master (pull request #27)
    coolip SESSION 위치 변경

---
+++ src/test/java/net/nurigo/java_sdk/examples/GroupMessage/ExampleDeleteGrouops.java
@@ -0,0 +1,29 @@
+package net.nurigo.java_sdk.examples.GroupMessage;
+
+import org.json.simple.JSONObject;
+
+import net.nurigo.java_sdk.api.GroupMessage;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+
+/**
+ * @class ExampleAddMessages
+ * @brief This sample code demonstrate how to delete sms group through CoolSMS Rest API PHP
+ */
+public class ExampleDeleteGrouops {
+	public static void main(String[] args) {
+		String api_key = "CS558104628ADED";
+		String api_secret = "983C21FB95000DCBD2A1C4FE25F14883";
+		GroupMessage coolsms = new GroupMessage(api_key, api_secret);		
+		
+		// group_ids are mandatory		
+		String group_ids = "GID56FA3B1BF0826, GID56FA3B1BF0836"; // Group IDs
+		
+		try {
+			JSONObject obj = (JSONObject) coolsms.deleteGroups(group_ids);
+			System.out.println(obj.toString());
+		} catch (CoolsmsException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getCode());
+		}
+	}
+}
