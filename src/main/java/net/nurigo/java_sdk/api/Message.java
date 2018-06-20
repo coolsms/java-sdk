17dc3812b23894951d0b4ebb8a81f03098260998
GeniusK <geniusk@rinc.kr>
2018-06-20 15:41:51 +0900

    gid mid -> group_id message_id

--- src/main/java/net/nurigo/java_sdk/api/Message.java
+++ src/main/java/net/nurigo/java_sdk/api/Message.java
@@ -77,8 +77,8 @@ public class Message extends Coolsms {
    * @throws CoolsmsException
    */
   public JSONObject cancel(HashMap<String, String> params) throws CoolsmsException {
-    if (!checkString(params.get("mid")) && !checkString(params.get("gid")))
-      throw new CoolsmsSDKException("mid or gid either one must be entered", 202);
+    if (!checkString(params.get("message_id")) && !checkString(params.get("group_id")))
+      throw new CoolsmsSDKException("message_id or group_id either one must be entered", 202);
 
     return sendPostRequest("cancel", params);
   }
